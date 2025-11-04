677c6ba14b5c4d2a834e090e53b22ee05a634cde
kharabeaniruddha <kharabeaniruddha@gmail.com>
2025-11-04 08:53:33 +0530

    created react using cdn

---
+++ app.js
@@ -0,0 +1,19 @@
+const h1Tag = React.createElement(
+  "h1",
+  { id: "title" },
+  "Hello, Welcome to React Js"
+);
+
+const h2Tag = React.createElement("h2", {}, "Lets Learn React");
+
+const headingDiv = React.createElement("div", { id: "heading" }, [
+  h1Tag,
+  h2Tag,
+]);
+
+console.log(headingDiv);
+
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(headingDiv);
