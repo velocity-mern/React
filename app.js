38a4c350acc70bef1d54ba1fa5365edc1422d722
kharabeaniruddha <kharabeaniruddha@gmail.com>
2025-11-06 08:57:16 +0530

    react functional component

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// ReactElemenet => Object => HTML Element
 const h1Tag = React.createElement(
   "h1",
   { id: "title" },
   "Hello, Welcome to React Js"
 );
 
-const h2Tag = React.createElement("h2", {}, "Lets Learn React");
+console.log("h1Tag", h1Tag);
 
-const headingDiv = React.createElement("div", { id: "heading" }, [
-  h1Tag,
-  h2Tag,
-]);
+// JSX // traspiling the code by PARCEL => Babel
 
-console.log(headingDiv);
+//JSX(code) => React.createElement => Object =>HTML Element
+
+// ReactElemenet
+const h2Tag = (
+  <div>
+    <h1 id="title">Hello, Welcome to React Js with JSX</h1>
+  </div>
+);
+
+console.log("h2Tag", h2Tag);
+
+//component
+// 1. functional based component => using function => new way of creating compnent
+// 2. class based component => class => older way of creating compnent
+
+//functional based component => it just function which return jsx
+
+const HeadingComponent = () => {
+  return (
+    <h1 id="title">
+      Hello, Welcome to React Js with JSX in Functional component
+    </h1>
+  );
+};
+
+const HeadingComponent2 = () => (
+  <div>
+    <h1 id="title"> this is only for testing purpose</h1>
+  </div>
+);
 
 const rootElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(headingDiv);
+root.render(<HeadingComponent2 />);
